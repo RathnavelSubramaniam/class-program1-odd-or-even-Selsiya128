7fd249cdb5324c715253a06aa67404cf8a576708
Selsiya128 <142964338+Selsiya128@users.noreply.github.com>
2023-09-11 12:38:24 +0530

    Create oddoreven.java

---
+++ oddoreven.java
@@ -0,0 +1,9 @@
+class oddoreven;
+public static void main(String args[]){
+int num = 4567
+if(num%2 ==0){
+system.out.println("The given number"+num+is true");
+else{
+system.out.println("the given number"+num+is odd");
+}
+}
